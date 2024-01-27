fb2c112e16bc71ec451b0f9e119a3eb94ece31a7
mor15Euro <moritz@bachmaiers.de>
2024-01-27 12:16:21 +0100

    Removed Flutter logs

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_logs/flutter_logs.dart';
+
 import 'package:smart_home/manager/manager.dart';
 
 import 'app.dart';
@@ -19,29 +19,7 @@ void main() async {
   HttpOverrides.global = MyHttpOverrides();
 
   //Initialize Logging
-  await FlutterLogs.initLogs(
-      logLevelsEnabled: [
-        LogLevel.INFO,
-        LogLevel.WARNING,
-        LogLevel.ERROR,
-        LogLevel.SEVERE,
-      ],
-      timeStampFormat: TimeStampFormat.TIME_FORMAT_READABLE,
-      directoryStructure: DirectoryStructure.FOR_DATE,
-      logTypesEnabled: [
-        "device",
-        "customise",
-        "connection",
-        "screen",
-        "iobrokerManager",
-        "error"
-      ],
-      logFileExtension: LogFileExtension.LOG,
-      logsWriteDirectoryName: "logs",
-      logsExportDirectoryName: "logs/Exported",
-      debugFileOperations: false,
-      attachTimeStamp: true,
-      isDebuggable: false);
+
   String version = "1.3";
   String buildNumber = "100";
   Manager manager = Manager();
