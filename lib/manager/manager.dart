dcc88690e90637832d76f8bae704acf679a59292
Moritz Bachmaier <moritz@bachmaiers.de>
2024-04-25 19:46:34 +0200

    prepared beta release

--- lib/manager/manager.dart
+++ lib/manager/manager.dart
@@ -23,7 +23,7 @@ import '../background/background_runner.dart';
 class Manager {
   //@Deprecated("Please use the Context")
   static final Manager instance =
-      Manager._internal(versionNumber: "0.0.620", buildNumber: "151");
+      Manager._internal(versionNumber: "0.0.621", buildNumber: "153");
   static final navigatorKey = GlobalKey<NavigatorState>();
   //@Deprecated("Please use the Context")
   factory Manager() => instance;
