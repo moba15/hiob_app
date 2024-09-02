f8539ddf311d6f2a97c1ea23af13c8ed27ed398d
Moritz Bachmaier <moritz@bachmaiers.de>
2024-09-02 14:25:58 +0200

    added changelog

--- lib/changelog/view/changelog_view.dart
+++ lib/changelog/view/changelog_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:smart_home/manager/cubit/manager_cubit.dart';
 import 'package:smart_home/manager/manager.dart';
 
@@ -9,115 +10,33 @@ class ChangeLogScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          manager.status = ManagerStatus.finished;
-          manager.managerStatusStreamController.sink
-              .add(ManagerStatus.finished);
-        },
-        child: const Icon(Icons.visibility),
-      ),
-      appBar: AppBar(
-        title: Text(
-            "What's new? ${manager.versionNumber}(${manager.buildNumber})"),
-      ),
-      body: ListView(
-        children: [
-          Align(
-            alignment: Alignment.center,
-            child: Column(
-              children: [
-                const Text(
-                  "Changelog ",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-                ),
-                Text(
-                  "Version: ${manager.versionNumber}",
-                  style: const TextStyle(fontWeight: FontWeight.normal),
-                )
-              ],
-            ),
-          ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            manager.status = ManagerStatus.finished;
+            manager.managerStatusStreamController.sink
+                .add(ManagerStatus.finished);
+          },
+          child: const Icon(Icons.visibility),
+        ),
+        appBar: AppBar(
+          title: Text(
+              "What's new? ${manager.versionNumber}(${manager.buildNumber})"),
+        ),
+        body: Markdown(
+          data: _changelog(),
+        ));
+  }
 
-          /*const Text("Widgets", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
-          Container(
-            margin: const EdgeInsets.only(top: 3, left: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text("- Added the Graph Widget (only use this widget if you backedup your current state):"),
-                Container(
-                  margin: const EdgeInsets.only(left: 15),
-                  child: const Text("If you use the sql Adapter you can now use this Widget to display a Graph"),
-                ),
-
-
-              ],
-            ),
-          ),*/
-
-          /* const Text(
-            "Notifications",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 3, left: 10),
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                    "- Logging for notification bug finding, see general settings"),
-                Text("- Clicking on Conncetion Status now will open the app"),
-                Text("- AES support added"),
-                Text("- Notification log now available (beta)"),
-                Text("- Fixed some bugs"),
-              ],
-            ),
-          ), */
-          const Text(
-            "Devices",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 3, left: 10),
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("- You can now override the status of the Device"),
-              ],
-            ),
-          ),
-          const Text(
-            "Color picker",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 3, left: 10),
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("- Now should work with all prefixes"),
-              ],
-            ),
-          ),
-          const Text(
-            "General",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 3, left: 10),
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "There is currently running a poll on GitHub under: github.com/moba15/hiob_app/discussions",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+  String _changelog() {
+    return """
+# Templates \n
+- **Important:** The new features are all highly experimental
+- I completly rewrote the way templates/widgtes work
+- You now can find two (new) widgets one of them a new input widget
+- With the new types of widgtes you can directly add a popupmenu, so you don't have to use a advanced widget for this
+- The idea is that the advanced widget is no longer needed, with these kind of widgets
+- In the future I will replace all old widgtes with the new widget types and give you a way to migrate your old widgtes to the new one
+- Please provide feadback over github or the iobroker forum
+  """;
   }
 }
