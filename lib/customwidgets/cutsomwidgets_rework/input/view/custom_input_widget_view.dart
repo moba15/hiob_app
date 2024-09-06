5274ec1deb75af0b8c5d44b92d06873d1eac9cd9
Moritz <moritz@bachmaiers.de>
2024-09-06 11:57:47 +0200

    input widget close bloc

--- lib/customwidgets/cutsomwidgets_rework/input/view/custom_input_widget_view.dart
+++ lib/customwidgets/cutsomwidgets_rework/input/view/custom_input_widget_view.dart
@@ -3,17 +3,35 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smart_home/customwidgets/cutsomwidgets_rework/input/custom_input_widget.dart';
 import 'package:smart_home/device/state/bloc/datapoint_bloc.dart';
 
-class CustomInputWidgetView extends StatelessWidget {
+class CustomInputWidgetView extends StatefulWidget {
   final CustomInputWidget customInputWidget;
   const CustomInputWidgetView({Key? key, required this.customInputWidget})
       : super(key: key);
 
   @override
+  State<CustomInputWidgetView> createState() => _CustomInputWidgetViewState();
+}
+
+class _CustomInputWidgetViewState extends State<CustomInputWidgetView> {
+  late DataPointBloc bloc;
+  @override
+  void initState() {
+    bloc = DataPointBloc(widget.customInputWidget.dataPoint!);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    bloc.close();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    if (customInputWidget.dataPoint == null) {
+    if (widget.customInputWidget.dataPoint == null) {
       return const Text("Device not found");
     }
-    DataPointBloc bloc = DataPointBloc(customInputWidget.dataPoint!);
 
     /*DataPointBloc bloc context.select<DataPointBloc, DataPointBloc>((value) {
       if (value.dataPoint == customInputWidget.dataPoint) {
@@ -26,23 +44,24 @@ class CustomInputWidgetView extends StatelessWidget {
       bloc: bloc,
       builder: (context, state) {
         TextEditingController textEditingController = TextEditingController();
-        if (customInputWidget.customInputDisplayConentType ==
+        if (widget.customInputWidget.customInputDisplayConentType ==
             CustomInputDisplayConentType.value) {
           textEditingController.text = state.value.toString();
         }
         const onChanged = null;
-        final onSubmitted = customInputWidget.customInputSendMethod ==
+        final onSubmitted = widget.customInputWidget.customInputSendMethod ==
                 CustomInputSendMethod.onSubmitted
             ? send
             : null;
-        final hintText = customInputWidget.customInputDisplayConentType ==
-                CustomInputDisplayConentType.hintText
-            ? state.value.toString()
-            : null;
-        final Text label =
-            customInputWidget.label == null || customInputWidget.label!.isEmpty
-                ? Text(customInputWidget.name)
-                : Text(customInputWidget.label!);
+        final hintText =
+            widget.customInputWidget.customInputDisplayConentType ==
+                    CustomInputDisplayConentType.hintText
+                ? state.value.toString()
+                : null;
+        final Text label = widget.customInputWidget.label == null ||
+                widget.customInputWidget.label!.isEmpty
+            ? Text(widget.customInputWidget.name)
+            : Text(widget.customInputWidget.label!);
         return TextField(
           controller: textEditingController,
           onChanged: (v) => onChanged != null ? onChanged(v, bloc) : null,
