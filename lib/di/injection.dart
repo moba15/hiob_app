ced17c968aef23cb1d35e24ca0f3594b54297db0
Moritz <msb@bachmaiers.de>
2026-03-22 20:27:06 +0100

    fix injection (test)

--- lib/di/injection.dart
+++ lib/di/injection.dart
@@ -1,9 +1,20 @@
 import 'package:get_it/get_it.dart';
-
-import 'injection.config.dart';
+import 'package:smart_home/services/logging/app_logger.dart';
+import 'package:smart_home/services/logging/logging_service.dart';
+import 'package:smart_home/utils/widgets/dropdown_search/cubit/cubit/async_search_cubit_factory.dart';
 
 final GetIt getIt = GetIt.instance;
 
 Future<void> configureDependencies() async {
-  await getIt.init();
+  if (!getIt.isRegistered<LoggingService>()) {
+    getIt.registerLazySingleton<LoggingService>(() => LoggingService.instance);
+  }
+  if (!getIt.isRegistered<AppLogger>()) {
+    getIt.registerLazySingleton<AppLogger>(() => getIt<LoggingService>());
+  }
+  if (!getIt.isRegistered<AsyncSearchCubitFactory>()) {
+    getIt.registerLazySingleton<AsyncSearchCubitFactory>(
+      () => AsyncSearchCubitFactory(logger: getIt<AppLogger>()),
+    );
+  }
 }
